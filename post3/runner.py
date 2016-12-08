65fd076d5d92f68f63c5aa9384ca2c419fd168f2
Jon Vlachogiannis <darksun4@gmail.com>
2016-12-07 17:43:46 -0800

    post3 uses new OANDAs API

--- post3/runner.py
+++ post3/runner.py
@@ -1,29 +1,46 @@
 """
 The main file that will evolve into our trading library
 """
-from datetime import datetime
-import oandapy
+import v20
+from datetime import datetime, timedelta
 
 OANDA_ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
-OANDA_ACCOUNT_ID = 11111 #put your access id here 
+OANDA_ACCOUNT_ID = '11111' #put your access id here 
 
 def main():
     print "------ System online -------", datetime.now()
-    oanda = oandapy.API(environment="practice",
-                        access_token=OANDA_ACCESS_TOKEN)
-    response = oanda.get_prices(instruments="EUR_USD")
-    prices = response.get("prices")
-    buy_price = prices[0].get("bid")
+    latest_price_time = (datetime.utcnow() - timedelta(seconds=15)).isoformat('T')+'Z'
 
-    print "Buy at", buy_price
+    api = v20.Context(
+            'api-fxpractice.oanda.com',
+            '443',
+            token=OANDA_ACCESS_TOKEN)
+
+    response = api.pricing.get(
+                    OANDA_ACCOUNT_ID,
+                    instruments='EUR_USD',
+                    since=latest_price_time,
+                    includeUnitsAvailable=False)
+
+    print response.reason, latest_price_time
     
-    trade_id = oanda.create_order(OANDA_ACCOUNT_ID, instrument="EUR_USD",
-                                  units=1000,
-                                  side='buy',
-                                  type='market')
+    prices = response.get("prices", 200)
+    if len(prices):
+        buy_price = prices[0].bids[0].price 
 
-    print "Trading id", trade_id
+        print "Buy at", buy_price
 
+        response = api.order.market(
+            OANDA_ACCOUNT_ID,
+            instrument='EUR_USD',
+            units=5000)
+
+        print "Trading id", response.get('orderFillTransaction').id
+        print "Account Balance", response.get('orderFillTransaction').accountBalance
+        print "Price", response.get('orderFillTransaction').price
+        
+    else:
+        print response.reason
 
 if __name__ == "__main__":
     main()
