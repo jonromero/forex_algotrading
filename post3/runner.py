34ed9f0eaa289f7f97f2af7ca907fa8ea22fe125
Corey Hu <coreyhu@berkeley.edu>
2017-10-12 11:43:47 -0700

    fixed TypeError regarding printing floats

--- post3/runner.py
+++ post3/runner.py
@@ -1,8 +1,9 @@
 """
 The main file that will evolve into our trading library
 """
-import v20
 from datetime import datetime, timedelta
+import v20
+
 
 OANDA_ACCESS_TOKEN = "01d4e6de4fc1246d2bfde02450871fe6-0ae159e87f2cc523a9116d9b535e85fa"
 OANDA_ACCOUNT_ID = '101-001-6963280-001' #put your access id here 
@@ -28,7 +29,7 @@ def main():
     if len(prices):
         buy_price = prices[0].bids[0].price 
 
-        print("Buy at" + buy_price)
+        print("Buy at" + str(buy_price))
 
         response = api.order.market(
             OANDA_ACCOUNT_ID,
